fc81b3bd7eebb50c851b662e9b77f61bd8c895e5
Eduards Bistrums <eduards.bistrums@gmail.com>
2022-04-21 16:49:56 +0300

    poolstr dont have to be comparable

--- pool_str.go
+++ pool_str.go
@@ -5,14 +5,14 @@ import (
 )
 
 // Creates new pool with any type provided
-func NewPoolStr[T comparable]() *PoolStr[T] {
+func NewPoolStr[T any]() *PoolStr[T] {
 	return &PoolStr[T]{
 		lock:    &sync.RWMutex{},
 		storage: make(map[string]T),
 	}
 }
 
-type PoolStr[T comparable] struct {
+type PoolStr[T any] struct {
 
 	// holds the data
 	storage map[string]T
